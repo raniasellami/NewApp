253e4afe5cd5ffe2248f31f853d244036f54386d
rania sellami <rania.sellmi@esprit.tn>
2022-10-26 11:45:21 +0200

    consommation du webservice

---
+++ lib/Src/service/ArticleService.dart
@@ -0,0 +1,13 @@
+import 'package:dio/dio.dart';
+import 'package:newsapp/Src/model/top_article_api_model.dart';
+
+class ArticleService {
+  final Dio _dio = Dio();
+  final _baseurl = "https://newsapi.org/v2";
+  final _apiKey = "6d897eccbbe546e49f4d1d93ba1dd965";
+  Future<TopArticleApiModel> Toparticles() async {
+    String endpoint = "$_baseurl top-headlines?country=us&apiKey=$_apiKey";
+    var response = await _dio.get(endpoint);
+    return TopArticleApiModel.fromJson(response.data);
+  }
+}
